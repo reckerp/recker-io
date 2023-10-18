5a026ebda0b433242bf7000b85c1ff3b8a1119fa
Paul Recker <github@paulrecker.de>
2023-10-18 18:30:57 +0200

    fix: change welcome image height on small devices

--- container/Hero/Hero.tsx
+++ container/Hero/Hero.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import images from '../../constants/images'
 import Image from 'next/image'
 import "./Hero.css";
-import {HeroReceipt} from "../../components";
+import { HeroReceipt } from "../../components";
 
 const Hero: React.FC = () => {
     return (
         <div className="app__hero">
             <div className="app__hero-welcome">
-                <Image src={images.welcome} alt="logo"/>
+                <Image src={images.welcome} alt="logo" />
             </div>
-            <HeroReceipt/>
+            <HeroReceipt />
         </div>
     );
 };
