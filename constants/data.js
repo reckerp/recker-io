72d98d0daa019053cfd9f548891a6191b1edde21
reckerp <git@recker.io>
2024-09-08 12:11:00 +0200

    feat: add project resqlme

--- constants/data.js
+++ constants/data.js
@@ -1,108 +1,109 @@
 import images from "./images";
 
 const projects = [
-    {
-        subTitle: "Next.js",
-        title: 'recker.io',
-        imgUrl: images.portfolio_image,
-        descr: 'Portfolio showcasing web development projects built with Next.js.',
-        gh_link: 'https://github.com/reckerp/recker-io',
-        demo_link: 'https://recker.io'
-    },
-    {
-        subTitle: "SwiftUI & Loopback4",
-        title: 'iTutorU',
-        imgUrl: images.itutoru_image,
-        descr: 'A modern and easy to use tutoring application built with Swift.',
-        gh_link: '',
-        demo_link: ''
-    },
-    {
-        subTitle: "Next.js & NextAuth.js",
-        title: 'Earmark',
-        imgUrl: images.earmark_image,
-        descr: 'Fullstack webapp to bookmark Spotify audiobooks.',
-        gh_link: 'https://github.com/reckerp/earmark',
-        demo_link: ''
-    },
-    {
-        subTitle: "Go & GinGonic",
-        title: 'Garrio',
-        imgUrl: images.project_example,
-        descr: 'A RESTful realtime API for anonymous chatrooms built with Go.',
-        gh_link: 'https://github.com/reckerp/garrio-server',
-        demo_link: ''
-    },
-    {
-        subTitle: "React",
-        title: 'Restaurant Website',
-        imgUrl: images.gericht_image,
-        descr: 'A frontend website designed for a restaurant and built with React.',
-        gh_link: 'https://github.com/reckerp/react-restaurant',
-        demo_link: 'https://reckerp.github.io/react-restaurant/'
-    },
-    {
-        subTitle: "PHP Webapplication",
-        title: 'EarShot',
-        imgUrl: images.earshot_image,
-        descr: 'Music streaming platform built for artists and listeners with PHP.',
-        gh_link: '',
-        demo_link: ''
-    },
-    {
-        subTitle: "Java Desktop Application",
-        title: 'Flight Management Tool',
-        imgUrl: images.flight_image,
-        descr: 'Flight management tool for airlines, built with JavaFX.',
-        gh_link: '',
-        demo_link: ''
-    },
-    {
-        subTitle: "Java Desktop Game",
-        title: 'BlackJack Simulator',
-        imgUrl: images.blackjack_image,
-        descr: 'Blackjack simulator created with Java Swing in a project course.',
-        gh_link: '',
-        demo_link: ''
-    },
-]
-
+  {
+    subTitle: "Next.js",
+    title: "recker.io",
+    imgUrl: images.portfolio_image,
+    descr: "Portfolio showcasing web development projects built with Next.js.",
+    gh_link: "https://github.com/reckerp/recker-io",
+    demo_link: "https://recker.io",
+  },
+  {
+    subTitle: "Next.js & Golang",
+    title: "resql.me",
+    imgUrl: images.resqlme_image,
+    descr:
+      "An interactive SQL learning platform, streamlining assessment and study.",
+    gh_link: "https://github.com/resqlme",
+    demo_link: "https://resql.me",
+  },
+  {
+    subTitle: "SwiftUI & Loopback4",
+    title: "iTutorU",
+    imgUrl: images.itutoru_image,
+    descr: "A modern and easy to use tutoring application built with Swift.",
+    gh_link: "",
+    demo_link: "",
+  },
+  {
+    subTitle: "Next.js & NextAuth.js",
+    title: "Earmark",
+    imgUrl: images.earmark_image,
+    descr: "Fullstack webapp to bookmark Spotify audiobooks.",
+    gh_link: "https://github.com/reckerp/earmark",
+    demo_link: "",
+  },
+  {
+    subTitle: "Go & GinGonic",
+    title: "Garrio",
+    imgUrl: images.project_example,
+    descr: "A RESTful realtime API for anonymous chatrooms built with Go.",
+    gh_link: "https://github.com/reckerp/garrio-server",
+    demo_link: "",
+  },
+  {
+    subTitle: "React",
+    title: "Restaurant Website",
+    imgUrl: images.gericht_image,
+    descr: "A frontend website designed for a restaurant and built with React.",
+    gh_link: "https://github.com/reckerp/react-restaurant",
+    demo_link: "https://reckerp.github.io/react-restaurant/",
+  },
+  {
+    subTitle: "PHP Webapplication",
+    title: "EarShot",
+    imgUrl: images.earshot_image,
+    descr: "Music streaming platform built for artists and listeners with PHP.",
+    gh_link: "",
+    demo_link: "",
+  },
+  {
+    subTitle: "Java Desktop Application",
+    title: "Flight Management Tool",
+    imgUrl: images.flight_image,
+    descr: "Flight management tool for airlines, built with JavaFX.",
+    gh_link: "",
+    demo_link: "",
+  },
+  {
+    subTitle: "Java Desktop Game",
+    title: "BlackJack Simulator",
+    imgUrl: images.blackjack_image,
+    descr: "Blackjack simulator created with Java Swing in a project course.",
+    gh_link: "",
+    demo_link: "",
+  },
+];
 
 const pl = [
-    "Java",
-    "JavaScript",
-    "TypeScript",
-    "Python",
-    "Swift",
-    "Go",
-    "SQL",
-    "",
-    "HTML",
-    "CSS"
-]
+  "Java",
+  "JavaScript",
+  "TypeScript",
+  "Python",
+  "Swift",
+  "Go",
+  "SQL",
+  "",
+  "HTML",
+  "CSS",
+];
 
-const tech = [
-    "Node.js",
-    "React",
-    "Next.js",
-    "Git",
-    "Docker",
-    "Postgres"
-]
+const tech = ["Node.js", "React", "Next.js", "Git", "Docker", "Postgres"];
 
 const other = [
-    "RESTful APIs",
-    "Backend development",
-    "Frontend development",
-    "Agile development (Scrum)",
-    "",
-    "German(native), English(C1)"
-]
+  "RESTful APIs",
+  "Backend development",
+  "Frontend development",
+  "Agile development (Scrum)",
+  "",
+  "German(native), English(C1)",
+];
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    projects,
-    pl,
-    tech,
-    other
-}
+  projects,
+  pl,
+  tech,
+  other,
+};
--- constants/images.js
+++ constants/images.js
@@ -1,37 +1,40 @@
 import logo from "../assets/logo.svg";
 import r_black from "../assets/r-black.svg";
-import hero_bg from "../assets/hero-background.svg"
-import welcome from "../assets/welcome.svg"
-import project_example from "../assets/rectangle.svg"
-import portfolio_image from "../assets/portfolio.png"
-import earshot_image from "../assets/earshot.png"
-import blackjack_image from "../assets/blackjack.png"
-import flight_image from "../assets/flight.png"
-import gericht_image from "../assets/gericht.png"
-import itutoru_image from "../assets/itutoru.png"
-import earmark_image from "../assets/earmark.png"
-import icon_linkedin from "../assets/icons/linkedin.svg"
-import icon_github from "../assets/icons/github.svg"
-import icon_instagram from "../assets/icons/instagram.svg"
-import icon_twitter from "../assets/icons/twitter.svg"
 
+import hero_bg from "../assets/hero-background.svg";
+import welcome from "../assets/welcome.svg";
+import project_example from "../assets/rectangle.svg";
+import portfolio_image from "../assets/portfolio.png";
+import earshot_image from "../assets/earshot.png";
+import blackjack_image from "../assets/blackjack.png";
+import flight_image from "../assets/flight.png";
+import gericht_image from "../assets/gericht.png";
+import itutoru_image from "../assets/itutoru.png";
+import earmark_image from "../assets/earmark.png";
+import resqlme_image from "../assets/resqlme.png";
+
+import icon_linkedin from "../assets/icons/linkedin.svg";
+import icon_github from "../assets/icons/github.svg";
+import icon_instagram from "../assets/icons/instagram.svg";
+import icon_twitter from "../assets/icons/twitter.svg";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    logo,
-    r_black,
-    hero_bg,
-    welcome,
-    icon_linkedin,
-    icon_github,
-    icon_instagram,
-    icon_twitter,
-    project_example,
-    portfolio_image,
-    earshot_image,
-    blackjack_image,
-    flight_image,
-    gericht_image,
-    itutoru_image,
-    earmark_image
-}
+  logo,
+  r_black,
+  hero_bg,
+  welcome,
+  icon_linkedin,
+  icon_github,
+  icon_instagram,
+  icon_twitter,
+  project_example,
+  portfolio_image,
+  earshot_image,
+  blackjack_image,
+  flight_image,
+  gericht_image,
+  itutoru_image,
+  earmark_image,
+  resqlme_image,
+};
